f40ccb1d9704a789bd3ef21195d39f47cb67e156
Serene Tu <serenemy@outlook.com>
2024-08-06 01:51:09 -0400

    update POMDP

--- POMDP_RL_decision_making/MATLAB/calc_belief_LR.m
+++
@@ -1,8 +0,0 @@
-function belief = calc_belief_LR(mu_t, sigma_t)
-    % This function calculates the beliefs for the true state being on left and
-    % right repectively.
-
-    bL = normcdf(0,mu_t,sigma_t);  % Left
-    bR = 1 - bL;                   % Right
-    belief = [bL, bR];
-end
